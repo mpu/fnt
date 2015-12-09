445504ced5d9856e547850d33ebb7cb43554b795
Quentin Carbonneaux <quentin.carbonneaux@yale.edu>
2015-12-09 14:41:41 -0500

    add a watch script

---
+++ src/watch.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if test -z $1
+then
+	echo "usage: watch.sh FONTPATH" 1>&2
+	exit 1
+fi
+
+echo "[+] Watching $1"
+
+while inotifywait -qr -o /dev/null -e modify "$1"
+do
+	echo "    reload ..."
+	killall -HUP view 2>/dev/null
+done
